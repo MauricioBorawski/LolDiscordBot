b5f10efbfb8ac0420fba048fc44cf876540bac6f
Mauricio Borawski <mauricio.borawski@gmail.com>
2021-08-13 20:21:01 -0300

    Add posibility to add /commands

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,19 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		await interaction.reply('Pong!');
-	},
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!")
+    .addStringOption((option) =>
+      option
+        .setName("input")
+        .setDescription("The input to echo back")
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    console.log(interaction.options.getString("input"));
+    await interaction.reply(`Pong!`);
+  },
 };
+
+//getString("input")) para responder a los input
--- index.js
+++ index.js
@@ -1,18 +1,46 @@
 // require dotenv
-require('dotenv').config();
+require("dotenv").config();
+const fs = require("fs");
 // require the needed discord.js classes
-const { Client, Intents } = require('discord.js');
-
+const { Client, Intents, Collection } = require("discord.js");
 
 // create a new Discord client
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
-client.once('ready', () => {
-	console.log('Ready!');
+client.once("ready", () => {
+  console.log("Ready!");
 });
 
+client.commands = new Collection();
+
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  // set a new item in the Collection
+  // with the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command);
+}
+
 // client.on('interactionCreate', );
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  if (!client.commands.has(interaction.commandName)) return;
+
+  try {
+    await client.commands.get(interaction.commandName).execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
+});
 
 client.login(`${process.env.DISCORD_TOKEN}`);
