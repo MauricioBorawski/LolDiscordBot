761028a1290045a25586b9cb8a73fc59277b8229
Mauricio Borawski <mauricio.borawski@gmail.com>
2021-08-17 00:12:31 -0300

    Add Flex, Solo and unranked support for the data

--- ranked-emblems/ranked_emblems.js
+++ ranked-emblems/ranked_emblems.js
@@ -1,13 +1,22 @@
 const ranked_emblem = {
-    iron: "https://cdn.discordapp.com/attachments/876320587839049800/876986212479553536/Emblem_Silver.png",
-    bronze: "https://cdn.discordapp.com/attachments/876320587839049800/876986190912450651/Emblem_Bronze.png",
-    silver: "https://cdn.discordapp.com/attachments/876320587839049800/876986212479553536/Emblem_Silver.png",
-    gold: "https://cdn.discordapp.com/attachments/876320587839049800/876986197895970856/Emblem_Gold.png",
-    platinum: "https://cdn.discordapp.com/attachments/876320587839049800/876986210806009896/Emblem_Platinum.png",
-    diamond: "https://cdn.discordapp.com/attachments/876320587839049800/876986197153574942/Emblem_Diamond.png",
-    master: "https://cdn.discordapp.com/attachments/876320587839049800/876986204799774730/Emblem_Master.png",
-    grandmaster: "https://cdn.discordapp.com/attachments/876320587839049800/876986200353812490/Emblem_Grandmaster.png",
-    challenger: "https://cdn.discordapp.com/attachments/876320587839049800/876986194385305610/Emblem_Challenger.png"
-}
+  unranked:
+    "https://static.wikia.nocookie.net/leagueoflegends/images/3/38/Season_2019_-_Unranked.png/revision/latest?cb=20190908074432",
+  iron: "https://cdn.discordapp.com/attachments/876320587839049800/876986212479553536/Emblem_Silver.png",
+  bronze:
+    "https://cdn.discordapp.com/attachments/876320587839049800/876986190912450651/Emblem_Bronze.png",
+  silver:
+    "https://cdn.discordapp.com/attachments/876320587839049800/876986212479553536/Emblem_Silver.png",
+  gold: "https://cdn.discordapp.com/attachments/876320587839049800/876986197895970856/Emblem_Gold.png",
+  platinum:
+    "https://cdn.discordapp.com/attachments/876320587839049800/876986210806009896/Emblem_Platinum.png",
+  diamond:
+    "https://cdn.discordapp.com/attachments/876320587839049800/876986197153574942/Emblem_Diamond.png",
+  master:
+    "https://cdn.discordapp.com/attachments/876320587839049800/876986204799774730/Emblem_Master.png",
+  grandmaster:
+    "https://cdn.discordapp.com/attachments/876320587839049800/876986200353812490/Emblem_Grandmaster.png",
+  challenger:
+    "https://cdn.discordapp.com/attachments/876320587839049800/876986194385305610/Emblem_Challenger.png",
+};
 
 module.exports = ranked_emblem;
--- utils/generateSearchMessege.js
+++ utils/generateSearchMessege.js
@@ -1,4 +1,5 @@
 const { MessageEmbed } = require("discord.js");
+const { silver } = require("../ranked-emblems/ranked_emblems");
 const ranked_emblem = require("../ranked-emblems/ranked_emblems");
 
 const capitalizeFirstLetter = (string) =>
@@ -12,34 +13,76 @@ const generateSearchMessege = (
   masteryChamps,
   bestChamps
 ) => {
-  return new MessageEmbed()
-    .setColor("#0099ff")
-    .setTitle(`${summoner_name}`)
-    .setURL(`https://las.op.gg/summoner/userName=${summoner_name}`)
-    .setThumbnail(
-      `http://ddragon.leagueoflegends.com/cdn/11.16.1/img/profileicon/${icon_number}.png`
-    )
-    .addFields(
-      { name: `Level: `, value: `${summoner_level}` },
-      { name: "\u200B", value: "\u200B" },
+  const soloQ = rank.find((data) => data.queueType === "RANKED_SOLO_5x5");
+  const flexQ = rank.find((data) => data.queueType === "RANKED_FLEX_SR");
+
+  const generateQueueForm = () => ({
+    soloQueue: [
       {
         name: `Division:`,
-        value: `${capitalizeFirstLetter(rank[1].tier)} ${rank[1].rank}`,
+        value: `${capitalizeFirstLetter(soloQ.tier)} ${soloQ.rank}`,
         inline: true,
       },
-      { name: `Puntos: `, value: `${rank[1].leaguePoints}`, inline: true },
+      { name: `Puntos: `, value: `${soloQ.leaguePoints}`, inline: true },
       {
         name: `Winrate:`,
         value: `${Math.round(
-          (rank[1].wins * 100) / (rank[1].wins + rank[1].losses)
+          (soloQ.wins * 100) / (soloQ.wins + soloQ.losses)
         )}%`,
         inline: true,
       },
-      { name: `Vicotorias:`, value: `${rank[1].wins}`, inline: true },
+      { name: `Vicotorias:`, value: `${soloQ.wins}`, inline: true },
       {
         name: `Partidas:`,
-        value: `${rank[1].wins + rank[1].losses}`,
-      }
+        value: `${soloQ.wins + soloQ.losses}`,
+      },
+    ],
+    flexQueue: [
+      {
+        name: `Division:`,
+        value: `${capitalizeFirstLetter(flexQ.tier)} ${flexQ.rank}`,
+        inline: true,
+      },
+      { name: `Puntos: `, value: `${flexQ.leaguePoints}`, inline: true },
+      {
+        name: `Winrate:`,
+        value: `${Math.round(
+          (flexQ.wins * 100) / (flexQ.wins + flexQ.losses)
+        )}%`,
+        inline: true,
+      },
+      { name: `Vicotorias:`, value: `${flexQ.wins}`, inline: true },
+      {
+        name: `Partidas:`,
+        value: `${flexQ.wins + flexQ.losses}`,
+      },
+    ],
+    unranked: [{ name: "Unranked", value: "Invocador no rankeado" }],
+  });
+
+  return new MessageEmbed()
+    .setColor("#0099ff")
+    .setTitle(`${summoner_name}`)
+    .setURL(
+      `https://las.op.gg/summoner/userName=${summoner_name
+        .replace(/\s/, "+")
+        .replace(/\s/g, "")}`
+    )
+    .setThumbnail(
+      `http://ddragon.leagueoflegends.com/cdn/11.16.1/img/profileicon/${icon_number}.png`
+    )
+    .addFields({ name: `Level: `, value: `${summoner_level}` })
+    .addFields(
+      { name: "Ranked:", value: "SoloQueue" },
+      generateQueueForm()[soloQ ? "soloQueue" : "unranked"].map(
+        (field) => field
+      )
+    )
+    .addFields(
+      { name: "Ranked:", value: "Flex Queue" },
+      generateQueueForm()[flexQ ? "flexQueue" : "unranked"].map(
+        (field) => field
+      )
     )
     .addFields(
       {
@@ -58,7 +101,17 @@ const generateSearchMessege = (
         inline: true,
       }
     )
-    .setImage(`${ranked_emblem[rank[1].tier.toLowerCase()]}`);
+    .setImage(
+      `${
+        ranked_emblem[
+          soloQ !== undefined
+            ? soloQ.tier.toLowerCase()
+            : flexQ !== undefined
+            ? flexQ.tier.toLowerCase()
+            : "unranked"
+        ]
+      }`
+    );
 };
 
 module.exports = generateSearchMessege;
