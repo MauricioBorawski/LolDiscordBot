d49a54ad591073ddabaf99c2de887cef7edc1255
Mauricio Borawski <mauricio.borawski@gmail.com>
2021-09-03 01:45:11 -0300

    Add types and install eslint

---
+++ types/responseTypes.ts
@@ -0,0 +1,42 @@
+export interface SummonerData {
+    accountId: string;
+    profileIconId: number;
+    summonerLevel: number;
+}
+
+export interface RankData {
+    queueType: string;
+    tier: string;
+    rank: string;
+    leaguePoints: number;
+    wins: number;
+    losses: number;
+    hotStreak: boolean;
+}
+
+export interface ChampionMastery {
+    championId: number;
+    championPoints: number;
+}
+
+export interface ChampionMasteryList {
+    data: ChampionMastery[];
+}
+
+export interface BannedChampions {
+    pickTurn: number;
+    championId: number;
+    teamId: 100 | 200;
+}
+
+export interface Participants {
+    championId: number;
+    teamId: 100 | 200;
+    summonerName: string;
+}
+
+export interface Spectator {
+    gameType: string;
+    bannedChampions: BannedChampions[];
+    participants: Participants[];
+}
